206fa0b496c482c7d34f0f023a27ac69dfa468d3
Marouane Saadaoui <your.email@example.com>
2025-05-23 22:41:42 +0100

    change

--- minimangol/parsing/tree/echo.c
+++ minimangol/parsing/tree/echo.c
@@ -6,7 +6,7 @@
 /*   By: marouane <marouane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 13:51:21 by marouane          #+#    #+#             */
-/*   Updated: 2025/05/23 13:51:38 by marouane         ###   ########.fr       */
+/*   Updated: 2025/05/23 22:40:16 by marouane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int ft_echo(char **args)
 
 	flag = 0;
 	i = 1;
-	if (!ft_strncmp(args[1], "-n", ft_strlen(args[1])))
+	if (args[1] && !ft_strncmp(args[1], "-n", ft_strlen(args[1])))
 	{
 		flag = 1;
 		i++;
